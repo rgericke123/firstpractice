9bc36c897a27796b63d00829de9ab2f1f7df0e2b
rgericke123 <88450263+rgericke123@users.noreply.github.com>
2021-08-04 12:19:05 -0400

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,23 @@
+/* CST8110 - Introduction to Computer Programming
+ * Section: 521
+ * Semester: 21S
+ * Professor: Morrison
+ * Student ID: 040768574
+ * Student Email: geri0004@algonquinlive.com
+ * Assignment 1
+ */   
+
+ public class Assignment1
+     {
+		public static void main(String[] args) 
+
+	String prefix = 040768579; 
+	int studentID = 40768579;
+	Int idRemainder = studentID % 10;
+
+		{
+		System.print.f(“000000000040768579\n”)
+		System.print.f(“prefix”, “ “, “idRemainder - studentID”)
+
+		}
+}.;[p
